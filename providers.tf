cd43bbc7931db0482de6bbe48334ff95336e7f65
Oluwatobie <tobi.j.lawal@gmail.com>
2026-06-03 13:04:29 +0100

    Fix terraform formatting

--- providers.tf
+++ providers.tf
@@ -5,11 +5,11 @@ terraform {
       version = "~> 3.0"
     }
   }
-  
+
   # This tells Terraform to store the state file in the Azure vault we just made
   backend "azurerm" {
     resource_group_name  = "rg-terraform-backend"
-    storage_account_name = "tfstateoluwatech13" 
+    storage_account_name = "tfstateoluwatech13"
     container_name       = "tfstate"
     key                  = "leicester.terraform.tfstate"
   }
